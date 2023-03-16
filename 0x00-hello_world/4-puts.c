88f54d71679962b881aecd96311298e59c8659d1
kaoutar-NAIMA <kaoutarnaima2000@gmail.com>
2023-03-16 15:49:10 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main-prints"Programmingislikebuildingamultilingualpuzzle" 
- * Return:0(Success)
+ * main - prints "Programming is like building a multilingual puzzle" 
+ * Return: Always 0.
  */
 
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types 
- * Return 0 (Success)
+ * main - Prints the size of various computer types
+ * Return:0 (Success)
  */
+
 int main(void)
 {
 char a;
@@ -10,9 +11,11 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long:%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char:%zu byte(s)\n", sizeof(a));
+printf("Size of an int:%zu byte(s)\n", sizeof(b));
+printf("Size of a long:%zu byte(s)\n", sizeof(c));
+printf("Size of a long long int:%zu byte(s)\n", sizeof(d));
+printf("Size of a float:%zu byte(s)\n", sizeof(f));
+return (0);
 }
+
