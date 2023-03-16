aacb01692ca257f7bc87f1819c057cb24213f9e6
kaoutar-NAIMA <kaoutarnaima2000@gmail.com>
2023-03-16 16:57:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful\"- Dora Korpar, 2015-10-19"
- * to the standard error
+ * main - Prints "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"
  * Return 1 (Success)
  */
 
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
