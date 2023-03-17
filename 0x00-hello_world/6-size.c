a6dee2d255fe29ef3aed2b05f63502b88a5d0de3
kaoutar-NAIMA <kaoutarnaima2000@gmail.com>
2023-03-16 19:24:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long d;
 float f;
-printf("Size of a char:%zu byte(s)\n", sizeof(a));
-printf("Size of an int:%zu byte(s)\n", sizeof(b));
-printf("Size of a long int:%zu byte(s)\n", sizeof(c));
-printf("Size of a long long:%zu byte(s)\n", sizeof(d));
-printf("Size of a float:%zu byte(s)\n", sizeof(f));
+printf("Size of a char:%zu byte(s)\n", (char) sizeof(a));
+printf("Size of an int:%zu byte(s)\n", (int)sizeof(b));
+printf("Size of a long int:%zu byte(s)\n", (long int)sizeof(c));
+printf("Size of a long long:%zu byte(s)\n", (long long)sizeof(d));
+printf("Size of a float:%zu byte(s)\n", (float)sizeof(f));
 return (0);
 }
