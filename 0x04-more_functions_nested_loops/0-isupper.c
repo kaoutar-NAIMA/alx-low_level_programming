22488e52c4daef76e6dce17ae632ff4fbe515fb0
kaoutar-NAIMA <kaoutarnaima2000@gmail.com>
2023-03-24 10:52:59 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  * _isupper - Check if the letter is upper or not
- * @x: the number to be checked
+ * @c: the number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
-int _isupper(int x)
+int _isupper(int c)
 {
-if (x >= 65 && x <= 90)
+if (c >= 65 && c <= 90)
 {
 return (1);
 }
